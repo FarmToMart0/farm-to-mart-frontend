405cfced68a340c36514f363d6cb837ddc581300
SumeelaMadusankha <hasumeela@gmail.com>
2022-09-20 10:24:35 +0530

    folder structure created and intialize

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import store from './reducers/store'
+import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import theme from './utils/theme'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+<ThemeProvider>  <App theme={theme}/></ThemeProvider>
+    
+ 
+ 
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
