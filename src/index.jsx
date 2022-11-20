ebe3fe423674775cf7da1a278e4ac1b119602979
SumeelaMadusankha <hasumeela@gmail.com>
2022-11-20 16:19:50 +0530

    pwa setup

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './reducers/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { ThemeProvider } from '@mui/material/styles';
-
 import theme from './utils/theme'
+import PWAInstallerPrompt from 'react-pwa-installer-prompt';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-<ThemeProvider theme={theme}><App /></ThemeProvider> 
+<ThemeProvider theme={theme}>
+<PWAInstallerPrompt 
+      render={({ onClick }) => (
+        <button type="button" onClick={onClick}> // make sure you pass onClick
+          Install
+        </button>
+      )}
+      callback={(data) => console.log(data)} 
+    />
+    <App />
+</ThemeProvider> 
 </Provider>
 );
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 reportWebVitals();
